08e38f44888abc375386600da1f5e8b400716972
Nayan <wizenheimer@proton.me>
2023-11-10 17:53:44 +0530

    feat: add schema validator

---
+++ src/middleware/validateResource.ts
@@ -0,0 +1,18 @@
+import { Request, Response, NextFunction } from "express";
+import { AnyZodObject } from "zod";
+
+
+const validate = (schema: AnyZodObject) => (req: Request, res: Response, next: NextFunction) => {
+    try {
+        // schema to be checked against
+        schema.parse({
+            body: req.body,
+            query: req.query,
+            params: req.params,
+        });
+    } catch (err: any) {
+        return res.status(400).send(err.errors);
+    }
+}
+
+export default validate;
