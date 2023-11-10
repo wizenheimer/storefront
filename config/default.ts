bbaeace890e03de253e9626f56bad71091f0a469
Nayan <wizenheimer@proton.me>
2023-11-10 18:15:12 +0530

    feat: add user model w/t validation

--- config/default.ts
+++ config/default.ts
@@ -5,4 +5,5 @@ dotenv.config();
 export default {
     port: 1337,
     dbUri: process.env.DEFAULT_DB_URI,
+    saltWorkFactor: 10,
 };
---
+++ src/model/user.model.ts
@@ -0,0 +1,56 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import config from "config";
+
+// interface for User Document
+export interface UserDocument extends mongoose.Document {
+    email: string;
+    name: string;
+    password: string;
+    createdAt: Date;
+    updatedAt: Date;
+    comparePassword(candidatePassword: string): Promise<boolean>;
+}
+
+// schema for mongoose
+const userSchema = new mongoose.Schema({
+    email: { type: String, required: true, unique: true },
+    name: { type: String, required: true },
+    password: { type: String, required: true },
+}, {
+    timestamps: true,
+});
+
+// incase password is modified
+// pre save hook updates password hash 
+userSchema.pre("save", async function (next) {
+    let user = this as UserDocument
+
+    if (!user.isModified('password')) {
+        return next();
+    }
+
+    const saltWorkFactor = config.get<number>("saltWorkFactor");
+    const salt = await bcrypt.genSalt(saltWorkFactor);
+
+    const hash = await bcrypt.hashSync(user.password, salt);
+    user.password = hash;
+
+    return next();
+});
+
+// compare passwords hash
+userSchema.methods.comparePassword = async function (candidatePassword: string): Promise<boolean> {
+    const user = this as UserDocument;
+    try {
+        await bcrypt.compare(candidatePassword, user.password);
+        return true;
+    } catch (err) {
+        return false;
+    }
+}
+
+// setup model from schema
+const UserModel = mongoose.model("User", userSchema);
+
+export default UserModel;
