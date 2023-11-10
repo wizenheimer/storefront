cf6dec1b1f49d8ed3d5b1af864db12628020edb7
Nayan <wizenheimer@proton.me>
2023-11-10 17:21:22 +0530

    feat: add server config

---
+++ config/default.ts
@@ -0,0 +1,8 @@
+import dotenv from "dotenv"
+
+dotenv.config();
+
+export default {
+    port: 1337,
+    dbUri: process.env.DEFAULT_DB_URI,
+};
---
+++ src/app.ts
@@ -0,0 +1,9 @@
+import express from "express"
+import config from "config";
+
+const port = config.get<number>("port")
+const app = express()
+
+app.listen(port, () => {
+    console.log("server started");
+});
