7c0a8a1bcbdaadc1ac03c64f5f9b0c685233a72a
Nayan <wizenheimer@proton.me>
2023-11-16 23:17:05 +0530

    feat: add product service

---
+++ src/service/product.service.ts
@@ -0,0 +1,31 @@
+import { FilterQuery, QueryOptions, UpdateQuery } from "mongoose";
+import ProductModel, { ProductDocument, ProductInput } from "../model/product.model";
+
+
+export async function createProduct(input: ProductInput) {
+    return ProductModel.create(input);
+}
+
+
+export async function findProduct(
+    query: FilterQuery<ProductDocument>,
+    options: QueryOptions = { lean: true },
+) {
+    return ProductModel.findOne(query, {}, options);
+}
+
+
+export async function findAndUpdateProduct(
+    query: FilterQuery<ProductDocument>,
+    update: UpdateQuery<ProductDocument>,
+    options: QueryOptions = { lean: true },
+) {
+    return ProductModel.findOneAndUpdate(query, update, options);
+}
+
+
+export async function deleteProduct(
+    query: FilterQuery<ProductDocument>,
+) {
+    return ProductModel.deleteOne(query);
+}
