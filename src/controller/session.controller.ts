4573f62b732701c9105f8dd730abd0a8758ac160
Nayan <wizenheimer@proton.me>
2023-11-16 23:03:48 +0530

    feat: add product model

--- src/controller/session.controller.ts
+++ src/controller/session.controller.ts
@@ -5,28 +5,28 @@ import { signJWT } from "../utils/jwt.utils";
 import config from "config";
 
 export async function createUserSessionHandler(req: Request, res: Response) {
-    // TODO: validate user password
+    // validate user password
     const user = await validatePassword(req.body);
     if (!user) {
         return res.status(401).send("Invalid Email or Password.");
     }
 
-    // TODO: create session
+    // create session
     const session = createSession(user._id, req.get("user-agent") || "")
 
-    // TODO: create access token
+    // create access token
     const accessToken = signJWT(
         { ...user, session: (await session)._id, },
         { expiresIn: config.get<string>("accessTokenTTL") }
     )
 
-    // TODO: create refresh token
+    // create refresh token
     const refreshToken = signJWT(
         { ...user, session: (await session)._id, },
         { expiresIn: config.get<string>("accessTokenTTL") }
     )
 
-    // TODO: return access and refresh token
+    // return access and refresh token
     return res.send({
         accessToken: accessToken,
         refreshToken: refreshToken,
