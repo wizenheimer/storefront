883b5dee688b74909ce3eed0a9d785017faf8754
Nayan <wizenheimer@proton.me>
2023-11-19 12:15:54 +0530

    feat: hook up routes

--- src/routes.ts
+++ src/routes.ts
@@ -5,13 +5,22 @@ import { createUserSchema } from "./schema/user.schema";
 import { createUserSessionHandler, deleteSessionHandler, getUserSessionsHandler } from "./controller/session.controller";
 import { createSessionSchema } from "./schema/session.schema";
 import requireUser from "./middleware/requireUser";
+import { createProductSchema, deleteProductSchema, getProductSchema, updateProductSchema } from "./schema/product.schema";
+import { createProductHandler, deleteProductHandler, getProductHandler, updateProductHandler } from "./controller/product.controller";
 
 function routes(app: Express) {
+    // health check routes
     app.get("/health", (req: Request, res: Response) => res.sendStatus(200));
+    // user and sessions route
     app.post("/api/users", validateResource(createUserSchema), createUserHandler);
     app.post("/api/sessions", validateResource(createSessionSchema), createUserSessionHandler);
     app.get("/api/sessions", requireUser, getUserSessionsHandler);
     app.delete("/api/sessions", requireUser, deleteSessionHandler);
+    // product routes
+    app.post("/api/products", [requireUser, validateResource(createProductSchema)], createProductHandler);
+    app.put("/api/products", [requireUser, validateResource(updateProductSchema)], updateProductHandler);
+    app.get("/api/products", [validateResource(getProductSchema)], getProductHandler);
+    app.delete("/api/products", [requireUser, validateResource(deleteProductSchema)], deleteProductHandler);
 }
 
 export default routes;
